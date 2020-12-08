d0ff532407a340f92b228c07d266946fa36a7e7e
x-a-n-d-e-r-k <xanderk@notbo.red>
2020-12-08 14:06:28 -0600

    Added product string based change script.

---
+++ change_service_to_specified_color_by_product.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+/* globals Meteor Session Services */
+function changeServicesToSpecifiedColorByProduct (lairServiceProduct, lairColor) {
+    // Changes the status of a given service to the specified color
+    //
+    // Based of changeServicesToSpecifiedColor, Created by: Dan Kottmann
+    // Updated by: Brook Keele
+    // Usage: changeServicesToSpecifiedColorByProduct('Microsoft HTTPAPI httpd 2.0', 'lair-orange')
+    // Colors available: lair-grey, lair-blue, lair-green, lair-orange, lair-red
+    // Requires client-side updates: true
+  
+    var projectId = Session.get('projectId')
+    var modifiedBy = Meteor.user().emails[0].address
+  
+    if (lairColor !== 'lair-grey' && lairColor !== 'lair-blue' && lairColor !== 'lair-green' && lairColor !== 'lair-orange' && lairColor !== 'lair-red') {
+      console.log('Invalid color specified')
+      return
+    }
+    var services = Services.find({
+      'projectId': projectId,
+      'product': lairServiceProduct
+    }).fetch()
+    if (services.length < 1) {
+      console.log('No services found')
+      return
+    }
+    services.forEach(function (service) {
+      Services.update({
+        '_id': service._id
+      }, {
+        $set: {
+          'status': lairColor,
+          'last_modifiedBy': modifiedBy
+        }
+      })
+    })
+    console.log('Total of ' + services.length + ' service(s) updated to ' + lairColor + '.')
+  }
+  
